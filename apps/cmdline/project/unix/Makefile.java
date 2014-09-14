7783b58433b0cac28468a628bddb1802a82d3808
PRR24 <prr@eava.ee>
2014-09-14 10:28:57 +0300

    Java näide kasutab default-java päiste teed, dokumentatsiooni kohandamine.

--- apps/cmdline/project/unix/Makefile.java
+++ apps/cmdline/project/unix/Makefile.java
@@ -1,7 +1,7 @@
 CC=g++
 FLAGS=-O2 -DNDEBUG -Wall -std=gnu++0x -fdata-sections -ffunction-sections -Wl,--gc-sections
 SOFLAGS=-fpic -shared -fno-strict-aliasing
-INCLUDE=-I /usr/lib/jvm/java-6-openjdk/include
+INCLUDE=-I /usr/lib/jvm/default-java/include
 FILES=../../java/*.cpp ../../../../lib/proof/*.cpp ../../../../lib/etana/*.cpp ../../../lib/*.cpp ../../../../lib/fsc/*.cpp ../../../../lib/fsc/fsjni/*.cpp
 MAP=-Wl,--version-script,../../java/linguistic_jni.map
 LIBS=-pthread -ldl
