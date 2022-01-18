f3a108c3c87473ec38e767199e475e45e04ab1ed
tv4om <tarmo.vaino@gmail.com>
2022-01-18 18:54:36 +0200

    Delete docker-elg directory

--- docker-elg/docker-elg-morf/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Based on example: https://gitlab.com/european-language-grid/usfd/simple-python-tokeniser
-
-exec /sbin/tini -- venv/bin/gunicorn --bind=0.0.0.0:7000 "--workers=$WORKERS" --worker-tmp-dir=/dev/shm "$@" server-fs-morph:app
