25aea86be2df8503c65dd479af868315e34a6634
merisiga <heiki-jaan.kaalep@ut.ee>
2022-02-02 16:21:27 +0200

    konnetaarides veidi täpitähti parandatud

--- lib/etana/sonatk.h
+++ lib/etana/sonatk.h
@@ -3,7 +3,7 @@
 #define SONATK_H
 
 /// @file sonatk.h
-/// @brief eesti k s�nade morf. anal��siga seot v�rk: prefiksite, sufiksite, vormide ja l�ppudega seot. konstandid ja struktuurid
+/// @brief eesti k sõnade morf. analüüsiga seot värk: prefiksite, sufiksite, vormide ja lõppudega seot. konstandid ja struktuurid
 
 #include "ini_mrf.h"
 #include "tmk2t.h"
@@ -11,36 +11,36 @@
 #include "post-fsc.h"
 #include "fsxstring.h"
 
-#define KOIK_LIIGID FSxSTR("ABCDFGHIJKNOPSUVWXYZ")     ///< v�imalikud s�naliigid
+#define KOIK_LIIGID FSxSTR("ABCDFGHIJKNOPSUVWXYZ")     ///< võimalikud sõnaliigid
 
-#define MITTE_VERB  FSxSTR("ABCDFGHIJKNOPSUWXYZ")      ///< v�imalikud s�naliigid, v.a. tegusona
+#define MITTE_VERB  FSxSTR("ABCDFGHIJKNOPSUWXYZ")      ///< võimalikud sõnaliigid, v.a. tegusona
 
 #define KI_LIIGID   FSxSTR("kABCDFGHIJKNOPSUVWXYZ")    ///< == KOIK_LIIGID + k ki-liite ad hoc variantide piiramiseks
 
-#define LIIK_VERB  FSxSTR("V")                          ///< s�naliik tegusona
+#define LIIK_VERB  FSxSTR("V")                          ///< sõnaliik tegusona
 
-#define LIIK_PARISNIMI  FSxSTR("H")                     ///< s�naliik p�risnimi
+#define LIIK_PARISNIMI  FSxSTR("H")                     ///< sõnaliik pärisnimi
 
-#define LIIK_YLDNIMI    FSxSTR("S")                     ///< s�naliik �ldnimi
+#define LIIK_YLDNIMI    FSxSTR("S")                     ///< sõnaliik üldnimi
 
-#define LIIK_SACU  FSxSTR("SACU")                       ///< s�naliigid SACU
+#define LIIK_SACU  FSxSTR("SACU")                       ///< sõnaliigid SACU
 
-#define LIIK_A  FSxSTR("A")                             ///< s�naliik omaduss�na; ainult A
+#define LIIK_A  FSxSTR("A")                             ///< sõnaliik omadussõna; ainult A
 
-#define LIIK_KAANDSONA  FSxSTR("ABCGHNOPSU")            ///< ainult k��ndsonad
+#define LIIK_KAANDSONA  FSxSTR("ABCGHNOPSU")            ///< ainult käändsõnad
 
-#define LIIK_KAAND1     FSxSTR("ABCDGHJKNOPSUXY")      ///< LIIK_KAANDSONA + muutumatud, v.a. h��ds�nad I
+#define LIIK_KAAND1     FSxSTR("ABCDGHJKNOPSUXY")      ///< LIIK_KAANDSONA + muutumatud, v.a. hüüdsõnad I
 
-#define TYLP_LIIK1  FSxSTR("ABCDGHIJKNOPSUWXYZ")        ///< gene jaoks - noomen, mis v�ib olla sg n
+#define TYLP_LIIK1  FSxSTR("ABCDGHIJKNOPSUWXYZ")        ///< gene jaoks - noomen, mis võib olla sg n
 
-#define TYLP_LIIK2  FSxSTR("ADGIJKPXYZ")                ///< gene jaoks - muutumatu s�na
+#define TYLP_LIIK2  FSxSTR("ADGIJKPXYZ")                ///< gene jaoks - muutumatu sõna
 
 
-#define ALGV_LIIK   FSxSTR("ABCHNOPSUV")                ///< algvormide leidmiseks - ainult neil on m�tet otsida algvormi 
+#define ALGV_LIIK   FSxSTR("ABCHNOPSUV")                ///< algvormide leidmiseks - ainult neil on mõtet otsida algvormi 
 
-#define SUFFE  6                                        ///< max arv �ksteise sisse sobivaid suf; oletus
+#define SUFFE  6                                        ///< max arv üksteise sisse sobivaid suf; oletus
 
-#define PREFFE  6                                       ///< max arv �ksteise sisse sobivaid pref; oletus
+#define PREFFE  6                                       ///< max arv üksteise sisse sobivaid pref; oletus
 
 #define SUVA_LP      -2                                 ///< == POLE_TAANDLP
 
@@ -48,17 +48,17 @@
 
 #define SUVA_SL      (FSxCHAR)'z'
 
-#define TABU_SL      (FSxCHAR)'t'                       ///< tabus�na (perse) tunnus
+#define TABU_SL      (FSxCHAR)'t'                       ///< tabusõna (perse) tunnus
 
 #define SPELL_SL     (FSxCHAR)'s'                       ///< mitmesõnalise nime viimase osa (Aires) tunnus (varem aint spelleris lubatud, 2015 alati lubatud) 
 
-#define TESA_SL      (FSxCHAR)'e'                       ///< aint tesauruses lubatava s�na (aukudega) tunnus 
+#define TESA_SL      (FSxCHAR)'e'                       ///< aint tesauruses lubatava sõna (aukudega) tunnus 
 
-#define MITTELIITUV_SL  (FSxCHAR)'m'                    ///< liits�na koosseisu mittesobiva s�na (viie_aastane) tunnus 
+#define MITTELIITUV_SL  (FSxCHAR)'m'                    ///< liitsõna koosseisu mittesobiva sõna (viie_aastane) tunnus 
 
-#define L_MITTELIITUV_SL  (FSxCHAR)'n'                    ///< liits�na l�ppu mittesobiva s�na (abt, bai, utt jms) tunnus 
+#define L_MITTELIITUV_SL  (FSxCHAR)'n'                    ///< liitsõna lõppu mittesobiva sõna (abt, bai, utt jms) tunnus 
 
-#define W_SL  (FSxCHAR)'W'                             ///< s�naliik deverbaal; ainult koos noomeniga (n�itab, et on tuletatud verbist, nt. jooks)
+#define W_SL  (FSxCHAR)'W'                             ///< sõnaliik deverbaal; ainult koos noomeniga (näitab, et on tuletatud verbist, nt. jooks)
 
 #define SUVA_VRM     -2
 
@@ -74,11 +74,11 @@
 
 #define LOEND_EL     500                                ///< max elementide arv 1 loendis
 
-#define V_STR_LEN    1024                               ///< v�ljundstingi max pikkus
+#define V_STR_LEN    1024                               ///< väljundstingi max pikkus
 
 #define MAG_SIZE     3                                  ///< sisend-magasini suurus
 
-#define MITU_OMASTAVAT  8                               ///< massiivi omastavad[][] m��tmed
+#define MITU_OMASTAVAT  8                               ///< massiivi omastavad[][] mõõtmed
 
 #define OMASTAVA_MAX_PIK 11
 
@@ -113,35 +113,35 @@ class KOMPONENT
 
         KOMPONENT *komp_jargmine;
         KOMPONENT *komp_eelmine;
-        FSXSTRING algsona;          ///< anal-tav s�na
-        int nihe;                   ///< komponendi algus s�nas
+        FSXSTRING algsona;          ///< anal-tav sõna
+        int nihe;                   ///< komponendi algus sõnas
         FSXSTRING k_algus;          ///< komponendi algus
         int k_pikkus;               ///< komponendi pikkus
         int k_tyyp;                 ///< PREFIKS, TYVI, SUFIKS v LOPP
 
-        // tuleneb sonastikust
+        // tuleneb sõnastikust
         int jrk_nr;                 ///< indeks vastavalt k_tyyp-ile kas
                                     ///< prefinfo, sufinfo v loppude tabelisse;
                                     ///< tuleneb sonastikust
-        int liitumistyyp;           ///< komponendi liitumis-liik; vajalik liits�nanduses;
+        int liitumistyyp;           ///< komponendi liitumis-liik; vajalik liitsõnanduses;
                                     ///< tuleneb sonastikust
 
-        // tuleneb komponentide sobivusn�uetest
+        // tuleneb komponentide sobivusnõuetest
         FSXSTRING sl;               ///< komponendi sonaliik;
-                                    ///< tuleneb komponentide sobivusn�uetest
+                                    ///< tuleneb komponentide sobivusnõuetest
         TYVE_INF tylgr;             ///< 1 element ty_lgrd[]-st;
-                                    ///< tuleneb komponentide sobivusn�uetest
+                                    ///< tuleneb komponentide sobivusnõuetest
         
         // ainult kui k_tyyp==LOPP
-        int lgr;                    ///< lopugrupp; ainult kui k_tyyp==LOPP
-        int lpnr;                   ///< lopu nr lopugrupis; ainult kui k_tyyp==LOPP
+        int lgr;                    ///< lõpugrupp; ainult kui k_tyyp==LOPP
+        int lpnr;                   ///< lopu nr lõpugrupis; ainult kui k_tyyp==LOPP
 
         // abi-info
         FSXSTRING liitumisinfo;     ///< abi-info
         FSXSTRING algvorm;          ///< abi-info
 		
-	int sonastikust;            ///< leitud s�nastikust v�i oletatud; 
-                                    ///< vaja lisakr�nksude ja algvormi leidmise jaoks
+	int sonastikust;            ///< leitud sõnastikust või oletatud; 
+                                    ///< vaja lisakrõnksude ja algvormi leidmise jaoks
 
     private:
         void InitClassVariables(void)
@@ -165,7 +165,7 @@ struct VARIANTIDE_AHEL
     /** ahela sisu: võimalikud komponendid */
     KOMPONENT *variant;                           
     struct VARIANTIDE_AHEL *eelmine_variant;    ///< eelmine ahel
-    struct VARIANTIDE_AHEL *jargmine_variant;   ///< j�rgmine ahel
+    struct VARIANTIDE_AHEL *jargmine_variant;   ///< järgmine ahel
     };
 
 void ahelad_vabaks(VARIANTIDE_AHEL **ahel);
