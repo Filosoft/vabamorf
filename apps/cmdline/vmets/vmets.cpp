2b9b259f438db1da92f97efbaf01343bae7fbddc
Tarmo Vaino <tarmo.vaino@gmail.com>
2022-03-14 14:45:18 +0200

    lisatud morf sünteesiprogramm vmets

---
+++ apps/cmdline/vmets/vmets.cpp
@@ -0,0 +1,7 @@
+#include "vmets.h"
+
+int Tmain(int argc, FSTCHAR** argv, FSTCHAR**envp)
+    {
+    //return MainTemplate<VMETA>(argc, argv, envp, NULL);
+    return MainTemplate<VMETS>(argc, argv, envp, NULL);
+    }
