4a82f5d1c97269fb9432dd814262ebf393daf1dc
Tarmo Vaino <tarmo.vaino@gmail.com>
2022-08-21 14:10:24 +0300

    poolele teel kuhugi

---
+++ lib/etana/fsjsoncpp.h
@@ -0,0 +1,42 @@
+#if !defined(FSJSONCPP_H)
+#define FSJSONCPP_H
+
+#include <jsoncpp/json/json.h> 
+#include <iostream>
+#include <string> 
+
+class FSJSONCPP
+{
+    public:
+        bool Parse(const char* str, std::string& message, Json::Value& jsonobj)
+        {
+            message = "";
+            if(jsonreader.parse(str, jsonobj) == false)
+            {
+                message = jsonreader.getFormattedErrorMessages().c_str();
+                return false;
+            }
+            return true;
+        }
+
+        void Writer(const Json::Value& jsonobj, bool use_StyledWriter = false)
+        {
+            if(use_StyledWriter)
+            {
+                Json::StyledWriter styled;
+                std::cout << styled.write(jsonobj) << std::endl;
+            }
+            else
+            {
+                Json::FastWriter fast;
+                std::cout << fast.write(jsonobj) << std::endl;
+            }
+        }
+
+    private:
+            Json::Reader jsonreader;
+               
+
+};
+
+#endif
