05efa29c0ec36556ac36c214c169a43004039a9f
Tarmo Vaino <tarmo.vaino@gmail.com>
2023-09-25 22:36:47 +0300

    flask_vmetsjson.py kohendatud

--- docker/flask_vmetsjson/flask_vmetsjson.py
+++ docker/flask_vmetsjson/flask_vmetsjson.py
@@ -1,6 +1,6 @@
  #!/usr/bin/env python3
 
-VERSION = "2023.09.20"
+VERSION = "2023.09.21"
 
 """ 
 ----------------------------------------------
@@ -22,9 +22,10 @@ Lähtekoodist pythoni skripti kasutamine
     $ venv/bin/python3 ./flask_vmetsjson.py
 1.4 CURLiga veebiteenuse kasutamise näited
     $ curl --silent --request POST --header "Content-Type: application/json" \
-        localhost:7008/api/generator/version | jq
+        localhost:7008/api/vm/generator/version | jq
     $ curl --silent --request POST --header "Content-Type: application/json" \
-        --data '{"params":{"vmetsjson":["--version"]}, "content":"tere"}'  localhost:7008/api/generator/process | jq
+        --data '{"params":{"vmetsjson":["--version"]}, "content":"tere"}' \
+        localhost:7008/api/vm/generator/process | jq
 ----------------------------------------------
 
 Lähtekoodist tehtud konteineri kasutamine
@@ -32,11 +33,11 @@ Lähtekoodist tehtud konteineri kasutamine
 2.1 Lähtekoodi allalaadimine: järgi punkti 1.1
 2.2 Konteineri kokkupanemine
     $ cd ~/git/vabamorf_github/docker/flask_vmetsjson
-    $ docker build -t tilluteenused/vmetsjson:2023.09.20 .
+    $ docker build -t tilluteenused/vmetsjson:2023.09.21 .
     # docker login -u tilluteenused
-    # docker push tilluteenused/vmetajson:2023.09.20 .
+    # docker push tilluteenused/vmetsjson:2023.09.21
 2.3 Konteineri käivitamine
-    $ docker run -p 7008:7008 tilluteenused/vmetsjson:2023.09.20
+    $ docker run -p 7008:7008 tilluteenused/vmetsjson:2023.09.21
 2.4 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
 
 ----------------------------------------------
@@ -44,7 +45,7 @@ Lähtekoodist tehtud konteineri kasutamine
 DockerHUBist tõmmatud konteineri kasutamine
 3 DockerHUBist koneineri tõmbamine (3.1), konteineri käivitamine (3.2) ja CURLiga veebiteenuse kasutamise näited (3.3)
 3.1 DockerHUBist konteineri tõmbamine
-    $ docker pull tilluteenused/vmetsjson:2023.09.20 
+    $ docker pull tilluteenused/vmetsjson:2023.09.21 
 3.2 Konteineri käivitamine: järgi punkti 2.3
 3.3 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
 
@@ -53,9 +54,16 @@ DockerHUBist tõmmatud konteineri kasutamine
 TÜ pilves töötava konteineri kasutamine
 4 CURLiga veebiteenuse kasutamise näited
     $ curl --silent --request POST --header "Content-Type: application/json" \
-        https://smart-search.tartunlp.ai/api/generator/version | jq  
-    TODO
-
+        https://smart-search.tartunlp.ai/api/vm/generator/version | jq  
+    $ curl --silent --request POST --header "Content-Type: application/json" \
+        --data '{"params":{"vmetsjson":["--version"]}, "content":"tere"}' \
+        https://smart-search.tartunlp.ai/api/vm/generator/process | jq
+    $ curl --silent --request POST --header "Content-Type: application/json" \
+        --data '{"params":{"vmetsjson":["--classic"]}, "content":"tere terre"}' \
+        https://smart-search.tartunlp.ai/api/vm/generator/process | jq
+    $ curl --silent --request POST --header "Content-Type: application/json" \
+        --data '{"params":{"vmetsjson":["--classic", "--guess"]}, "content":"tere terre"}' \
+        https://smart-search.tartunlp.ai/api/vm/generator/process | jq
 ----------------------------------------------
 """
 
@@ -72,7 +80,7 @@ proc = subprocess.Popen(['./vmetsjson', '--path=.'],
 
 app = Flask("vmetsjson")
 
-@app.route('/api/generator/version', methods=['GET', 'POST'])
+@app.route('/api/vm/generator/version', methods=['GET', 'POST'])
 @app.route('/version', methods=['GET', 'POST'])
 def flask_estnltk_version():
     """Tagastame veebiliidese versiooni
@@ -82,7 +90,7 @@ def flask_estnltk_version():
     """
     return jsonify({"version":VERSION})
 
-@app.route('/api/generator/process', methods=['POST'])
+@app.route('/api/vm/generator/process', methods=['POST'])
 @app.route('/process', methods=['POST'])
 def morf():
     """Morf sünteesime JSONiga nõutud lemmade+vormid ja kuvame tulemust JSONkujul
