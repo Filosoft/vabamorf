d3e601879b6547d5d81e701fc31d3b616d1669b5
Tarmo Vaino <tarmo.vaino@gmail.com>
2024-01-23 22:52:38 +0200

    veakäsitlust kohendatud

--- docker/flask_estnltk_sentok/flask_estnltk_sentok.py
+++ docker/flask_estnltk_sentok/flask_estnltk_sentok.py
@@ -9,7 +9,11 @@ Flask veebiserver, pakendab ESTNLTK sõnestaja veebiteenuseks
 
 ----------------------------------------------
 
-Lähtekoodist pythoni skripti kasutamine
+Lähtekoodist pythoni skripti käivitamine
+TODO
+
+----------------------------------------------
+Lähtekoodist käivitatud veebiserveri kasutamine
 1 Lähtekoodi allalaadimine (1.1), virtuaalkeskkonna loomine (1.2), veebiteenuse käivitamine pythoni koodist (1.3) ja CURLiga veebiteenuse kasutamise näited (1.4)
 1.1 Lähtekoodi allalaadimine
     $ mkdir -p ~/git/ ; cd ~/git/
@@ -23,14 +27,11 @@ Lähtekoodist pythoni skripti kasutamine
 1.4 CURLiga veebiteenuse kasutamise näited
     $ curl --silent --request POST --header "Content-Type: application/json" \
         --data '{"content":"Mees peeti kinni. Sarved&Sõrad: telef. +372 345 534."}' \
-        localhost:6000/process | jq
-    $ curl --silent --request POST --header "Content-Type: application/json" \
-        --data '{"content":"Mees peeti kinni. Sarved&Sõrad: telef. +372 345 534."}' \
-        localhost:6000/api/tokenizer/process | jq
+        localhost:6000/api/estnltk/tokenizer/process | jq
     $ curl --silent --request POST --header "Content-Type: application/json"  \
         localhost:6000/version | jq
     $ curl --silent --request POST --header "Content-Type: application/json"  \
-        localhost:6000/api/tokenizer/version | jq
+        localhost:6000/api/estnltk/tokenizer/version | jq
 
 ----------------------------------------------
 
@@ -39,9 +40,9 @@ Lähtekoodist tehtud konteineri kasutamine
 2.1 Lähtekoodi allalaadimine: järgi punkti 1.1
 2.2 Konteineri kokkupanemine
     $ cd ~/git/vabamorf_github/docker/flask_estnltk_sentok
-    $ docker build -t tilluteenused/estnltk_sentok:2023.04.18 .
+    $ docker build -t tilluteenused/api_estnltk_sentok:2023.04.18 .
     # docker login -u tilluteenused
-    # docker push tilluteenused/estnltk_sentok:2023.04.18   
+    # docker push tilluteenused/api_estnltk_sentok:2023.04.18   
 2.3 Konteineri käivitamine
     $ docker run -p 6000:6000 tilluteenused/estnltk_sentok:2023.04.18
 2.4 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
@@ -61,47 +62,92 @@ TÜ pilves töötava konteineri kasutamine
 4 CURLiga veebiteenuse kasutamise näited
     $ curl --silent --request POST --header "Content-Type: application/json" \
         --data '{"content":"Mees peeti kinni. Sarved&Sõrad: telef. +372 345 534."}' \
-        https://smart-search.tartunlp.ai/api/tokenizer/process | jq
+        https://smart-search.tartunlp.ai/api/estnltk/tokenizer/process | jq
     $ curl --silent --request POST --header "Content-Type: application/json" \
-        https://smart-search.tartunlp.ai/api/tokenizer/version | jq  
+        https://smart-search.tartunlp.ai/api/estnltk/tokenizer/version | jq  
 
 ----------------------------------------------
 """
 
+import os
 import subprocess
 import json
 import argparse
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
+from functools import wraps
 
 import estnltk_sentok
 
 app = Flask("estnltk_sentok")
 
-@app.route('/api/tokenizer/version', methods=['GET', 'POST'])
+# JSONsisendi max suuruse piiramine {{
+try:
+    MAX_CONTENT_LENGTH=int(os.environ.get('MAX_CONTENT_LENGTH'))
+except:
+    MAX_CONTENT_LENGTH = 5 * 1000000000 # 5 GB 
+
+def limit_content_length(max_length):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            cl = request.content_length
+            if cl is not None and cl > max_length:
+                abort(413)
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
+# }}
+
+@app.errorhandler(413) # Request Entity Too Large: The data value transmitted exceeds the capacity limit.
+def request_entity_too_large(e):
+    return jsonify(error=str(e)), 413
+
+@app.errorhandler(404) # The requested URL was not found on the server.
+def page_not_found(e):
+    return jsonify(error=str(e)), 404
+
+@app.errorhandler(400) # Rotten JSON
+def rotten_json(e):
+    return jsonify(error=str(e)), 400
+
+@app.errorhandler(500) # Internal Error
+def rotten_json(e):
+    return jsonify(error=str(e)), 500
+
+
+@app.route('/api/estnltk/tokenizer/version', methods=['GET', 'POST'])
 @app.route('/version', methods=['GET', 'POST'])
+@limit_content_length(MAX_CONTENT_LENGTH)
 def flask_estnltk_version():
     """Tagastame veebiliidese versiooni
 
     Returns:
         ~flask.Response: JSONkujul versioonistring
     """
-    return jsonify({"version":VERSION})
+    return jsonify({"FLASK-liidese version":VERSION})
 
-@app.route('/api/tokenizer/process', methods=['POST'])
-@app.route('/process', methods=['POST']) #@app.route('/morf', methods=['GET', 'POST'])
+@app.route('/api/estnltk/tokenizer/process', methods=['POST'])
+@app.route('/process', methods=['POST'])
+@limit_content_length(MAX_CONTENT_LENGTH)
 def flask_estnltk_sentok():
     """Lausestame ja sõnestame sisendteksti
 
     Returns:
         ~flask.Response: Lausestamise ja sõnestamise tulemused
     """
-    if "content" not in request.json:
-        request.json["warning"] = ["JSON ei sisalda sõnestatavat teksti (puudub 'content')"]
-        return jsonify(request.json)
-    if "annotations" not in request.json:
-        request.json["annotations"] = {}
-    request.json["annotations"]["sentences"], request.json["annotations"]["tokens"] = estnltk_sentok.estnltk_sentok(request.json["content"])
-    return jsonify(request.json)
+    try:
+        request_json = json.loads(request.data)
+    except ValueError as e:
+        abort(400, description=str(e))
+    if "content" not in request_json:
+        abort(400, description="Missing 'content'")
+    if "annotations" not in request_json:
+        request_json["annotations"] = {}
+    try:
+        request_json["annotations"]["sentences"], request_json["annotations"]["tokens"] = estnltk_sentok.estnltk_sentok(request_json["content"])
+    except ValueError as e:
+        abort(500, description=str(e))
+    return jsonify(request_json), 200
 
 if __name__ == '__main__':
     default_port=6000
