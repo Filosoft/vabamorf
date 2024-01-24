bef709b959566b3349e138e68e3400677ef7c55b
Tarmo Vaino <tarmo.vaino@gmail.com>
2024-01-24 13:38:25 +0200

    veakäsitlus+docker-compose.yml

--- docker/flask_stlspeller/flask_stlspeller.py
+++ docker/flask_stlspeller/flask_stlspeller.py
@@ -1,7 +1,5 @@
  #!/usr/bin/env python3
 
-VERSION = "2024.01.04"
-
 """
 ----------------------------------------------
 
@@ -9,8 +7,7 @@ Flask veebiserver, pakendab spelleri veebiteenuseks
 
 ----------------------------------------------
 
-Lähtekoodist pythoni skripti kasutamine
-1 Lähtekoodi allalaadimine (1.1), virtuaalkeskkonna loomine (1.2), veebiteenuse käivitamine pythoni koodist (1.3) ja CURLiga veebiteenuse kasutamise näited (1.4)
+1 Lähtekoodist käivitatud veebiserveri kasutamine
 1.1 Lähtekoodi allalaadimine
     $ mkdir -p ~/git/ ; cd ~/git/
     $ git clone git@github.com:Filosoft/vabamorf.git vabamorf_github
@@ -18,51 +15,52 @@ Lähtekoodist pythoni skripti kasutamine
     $ cd ~/git/vabamorf_github/docker/flask_stlspeller
     $ ./create_venv.sh
 1.3 Veebiserveri käivitamine pythoni koodist
+Kasutab UBUNTU 22.04 LTS eelkompileeritud programmi `stlspellerjson`.
+    $ cd ~/git/vabamorf_github/docker/flask_estnltk_sentok
     $ venv/bin/python3 ./flask_stlspeller.py
-1.4 CURLiga veebiteenuse kasutamise näited
-    $ curl --silent  --request POST --header "Content-Type: application/json" \
-        --data '{"content":"oun terre tere"}' \
+1.4 Kasutusnäited
+    $ curl --silent --request POST --header "Content-Type: application/json" \
+        --data '{"content":"oun õun terre"}' \
         localhost:7005/api/vm/speller/process | jq
-    $ curl --silent  --request POST --header "Content-Type: application/json" \
+    $ curl --silent --request POST --header "Content-Type: application/json" \
         localhost:7005/api/vm/speller/version | jq
-    $ curl --silent  --request POST --header "Content-Type: application/json" \
-        --data '{"params":{"stlspellerjson":["--version"]}}' \
-        localhost:7005/api/vm/speller/process | jq
-
+    $ curl --silent --request POST --header "Content-Type: application/json" \
+        --data '{"params":{"stlspellerjson":["--version"]}, "content":"" }' \
+        localhost:7005/api/vm/speller/process | jq     
 ----------------------------------------------
 
-Lähtekoodist tehtud konteineri kasutamine
-2 Lähtekoodi allalaadimine (2.1), konteineri kokkupanemine (2.2), konteineri käivitamine (2.3) ja CURLiga veebiteenuse kasutamise näited  (2.4)
+2 Lähtekoodist konteineri tegemine ja kasutamine
 2.1 Lähtekoodi allalaadimine: järgi punkti 1.1
 2.2 Konteineri kokkupanemine
     $ cd ~/git/vabamorf_github/docker/flask_stlspeller
-    $ DOCKER_BUILDKIT=1 docker build -t tilluteenused/vm_speller:2024.01.04 .
+    $ docker-compose build
     # docker login -u tilluteenused
-    # docker push tilluteenused/vm_speller:2024.01.04
+    # docker-compose push
 2.3 Konteineri käivitamine
-    $ docker run -p 7005:7005 tilluteenused/vm_speller:2024.01.04
+    $ docker-compose up -d
 2.4 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
+2.5 Konteineri peatamine
+    $ docker-compose down
 
 ----------------------------------------------
 
-DockerHUBist tõmmatud konteineri kasutamine
-3 DockerHUBist koneineri tõmbamine (3.1), konteineri käivitamine (3.2) ja CURLiga veebiteenuse kasutamise näited (3.3)
-3.1 DockerHUBist konteineri tõmbamine
-    $ docker pull tilluteenused/vm/speller:2024.01.04
-3.2 Konteineri käivitamine: järgi punkti 2.3
-3.3 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
+3 DockerHUBist tõmmatud konteineri kasutamine
+3.1 DockerHUBist konteineri tõmbamine ja käivitamine
+    $ docker-compose pull
+3.2 Konteineri käivitamine: järgi punkti 3.3
+3.3 CURLiga veebiteenuse kasutamise näited: järgi punkti 2.4
 
 ----------------------------------------------
 
-TÜ pilves töötava konteineri kasutamine
-4 CURLiga veebiteenuse kasutamise näited
+
+TÜ pilves töötava konteineri CURLiga kasutamise näited
     $ curl --silent --request POST --header "Content-Type: application/json" \
         --data '{"content":"oun õun terre"}' \
         https://smart-search.tartunlp.ai/api/vm/speller/process | jq
     $ curl --silent --request POST --header "Content-Type: application/json" \
         https://smart-search.tartunlp.ai/api/vm/speller/version | jq  
     $ curl --silent --request POST --header "Content-Type: application/json" \
-        --data '{"params":{"stlspellerjson":["--version"]}}' \
+        --data '{"params":{"stlspellerjson":["--version"]}, "content":"" }' \
         https://smart-search.tartunlp.ai/api/vm/speller/process | jq 
 ----------------------------------------------
 
@@ -71,7 +69,7 @@ TÜ pilves töötava konteineri kasutamine
 5.1 Vaikeväärtustega `deployment`-konfiguratsioonifaili loomine
 
 kubectl create deployment smart-search-api-vm-speller \
-  --image=tilluteenused/vm_speller:2024.01.04
+  --image=tilluteenused/api_vm_speller:2024.01.23
 
 5.2 Vaikeväärtustega `service`-konfiguratsioonifaili loomine
 
@@ -93,11 +91,12 @@ path: /api/vm/speller/?(.*)
 pathType: Prefix
 
 """
-
+import os
 import subprocess
 import json
 import argparse
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
+from functools import wraps
 
 proc = subprocess.Popen(['./stlspellerjson', '--path=.'],  
                             universal_newlines=True, 
@@ -107,29 +106,80 @@ proc = subprocess.Popen(['./stlspellerjson', '--path=.'],
 
 app = Flask("stlspellerjson")
 
+VERSION = "2024.01.23"
+
+# JSONsisendi max suuruse piiramine {{
+try:
+    MAX_CONTENT_LENGTH=int(os.environ.get('MAX_CONTENT_LENGTH'))
+except:
+    MAX_CONTENT_LENGTH = 5 * 1000000000 # 5 GB 
+
+def limit_content_length(max_length):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            cl = request.content_length
+            if cl is not None and cl > max_length:
+                abort(413)
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
+# }}
+
+@app.errorhandler(413) # Request Entity Too Large: The data value transmitted exceeds the capacity limit.
+def request_entity_too_large(e):
+    return jsonify(error=str(e)), 413
+
+@app.errorhandler(404) # The requested URL was not found on the server.
+def page_not_found(e):
+    return jsonify(error=str(e)), 404
+
+@app.errorhandler(400) # Rotten JSON
+def rotten_json(e):
+    return jsonify(error=str(e)), 400
+
+@app.errorhandler(500) # Internal Error
+def rotten_json(e):
+    return jsonify(error=str(e)), 500
+
+#---------------------------------------------------------------------------
+
+
 @app.route('/api/vm/speller/version', methods=['GET', 'POST'])
 @app.route('/api/speller/version', methods=['GET', 'POST'])
 @app.route('/version', methods=['GET', 'POST'])
+@limit_content_length(MAX_CONTENT_LENGTH)
 def version():
     """Tagastame veebiliidese versiooni
 
     Returns:
         ~flask.Response: JSONkujul versioonistring
     """
-    return jsonify({"FLASK-liidese version":VERSION})
+    return jsonify({"FLASK-liidese version":VERSION, "MAX_CONTENT_LENGTH": MAX_CONTENT_LENGTH}), 200
 
 @app.route('/api/vm/speller/process', methods=['POST'])
 @app.route('/api/speller/process', methods=['POST'])
 @app.route('/process', methods=['POST'])
+@limit_content_length(MAX_CONTENT_LENGTH)
 def speller():
     """Kontrollime õigekirja ja pakume soovitusi
 
     Returns:
         ~flask.Response: Tulemused
     """
-    proc.stdin.write(f'{json.dumps(request.json)}\n')
-    proc.stdin.flush()
-    return jsonify(json.loads(proc.stdout.readline()))
+    try:
+        request_json = json.loads(request.data)
+    except ValueError as e:
+        abort(400, description=str(e))
+    if "content" not in request_json:
+        abort(400, description="Missing 'content'")
+    try:    
+        proc.stdin.write(f'{json.dumps(request_json)}\n')
+        proc.stdin.flush()
+        response_json = json.loads(proc.stdout.readline())
+    except ValueError as e:
+        abort(500, description=str(e))    
+    return jsonify(response_json), 200
 
 if __name__ == '__main__':
     default_port=7005
