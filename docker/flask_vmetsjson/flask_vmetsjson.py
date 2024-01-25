5ca5a9cee3eef9fd34f06b00c5157f6b90c05239
Tarmo Vaino <tarmo.vaino@gmail.com>
2024-01-25 09:32:53 +0200

    üht,teist

--- docker/flask_vmetsjson/flask_vmetsjson.py
+++ docker/flask_vmetsjson/flask_vmetsjson.py
@@ -1,7 +1,5 @@
  #!/usr/bin/env python3
 
-VERSION = "2023.09.21"
-
 """ 
 ----------------------------------------------
 
@@ -9,8 +7,7 @@ Flask veebiserver, pakendab Filosofti morfoloogilise süntesaatori veebiteenusek
 
 ----------------------------------------------
 
-Lähtekoodist pythoni skripti kasutamine
-1 Lähtekoodi allalaadimine (1.1), virtuaalkeskkonna loomine (1.2), veebiteenuse käivitamine pythoni koodist (1.3) ja CURLiga veebiteenuse kasutamise näited (1.4)
+1 Lähtekoodist käivitatud veebiserveri kasutamine
 1.1 Lähtekoodi allalaadimine
     $ mkdir -p ~/git/ ; cd ~/git/
     $ git clone git@github.com:Filosoft/vabamorf.git vabamorf_github
@@ -18,6 +15,7 @@ Lähtekoodist pythoni skripti kasutamine
     $ cd ~/git/vabamorf_github/docker/flask_vmetsjson
     $ ./create_venv.sh
 1.3 Veebiserveri käivitamine pythoni koodist
+Kasutab UBUNTU 22.04 LTS peal eelkompileeritud programmi `vmetsjson`.
     $ cd ~/git/vabamorf_github/docker/flask_vmetsjson
     $ venv/bin/python3 ./flask_vmetsjson.py
 1.4 CURLiga veebiteenuse kasutamise näited
@@ -28,31 +26,31 @@ Lähtekoodist pythoni skripti kasutamine
         localhost:7008/api/vm/generator/process | jq
 ----------------------------------------------
 
-Lähtekoodist tehtud konteineri kasutamine
-2 Lähtekoodi allalaadimine (2.1), konteineri kokkupanemine (2.2), konteineri käivitamine (2.3) ja CURLiga veebiteenuse kasutamise näited  (2.4)
+2 Lähtekoodist tehtud konteineri kasutamine
 2.1 Lähtekoodi allalaadimine: järgi punkti 1.1
 2.2 Konteineri kokkupanemine
     $ cd ~/git/vabamorf_github/docker/flask_vmetsjson
-    $ docker build -t tilluteenused/vmetsjson:2023.09.21 .
+    $ docker-compose build
     # docker login -u tilluteenused
-    # docker push tilluteenused/vmetsjson:2023.09.21
+    # docker-compose push
 2.3 Konteineri käivitamine
-    $ docker run -p 7008:7008 tilluteenused/vmetsjson:2023.09.21
+    $  docker-compose up -d
 2.4 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
+2.5 Konteineri peatamine
+    $ docker-compose down
 
 ----------------------------------------------
 
-DockerHUBist tõmmatud konteineri kasutamine
-3 DockerHUBist koneineri tõmbamine (3.1), konteineri käivitamine (3.2) ja CURLiga veebiteenuse kasutamise näited (3.3)
-3.1 DockerHUBist konteineri tõmbamine
-    $ docker pull tilluteenused/vmetsjson:2023.09.21 
-3.2 Konteineri käivitamine: järgi punkti 2.3
-3.3 CURLiga veebiteenuse kasutamise näited: järgi punkti 1.4
+3 DockerHUBist tõmmatud konteineri kasutamine
+3.1 DockerHUBist konteineri tõmbamine ja käivitamine
+    $ docker-compose pull
+3.2 Konteineri käivitamine: järgi punkti 3.3
+3.3 CURLiga veebiteenuse kasutamise näited: järgi punkti 2.4
 
 ----------------------------------------------
 
-TÜ pilves töötava konteineri kasutamine
-4 CURLiga veebiteenuse kasutamise näited
+4 TÜ pilves töötava konteineri kasutamise näited
+
     $ curl --silent --request POST --header "Content-Type: application/json" \
         https://smart-search.tartunlp.ai/api/vm/generator/version | jq  
     $ curl --silent --request POST --header "Content-Type: application/json" \
@@ -64,13 +62,48 @@ TÜ pilves töötava konteineri kasutamine
     $ curl --silent --request POST --header "Content-Type: application/json" \
         --data '{"params":{"vmetsjson":["--classic", "--guess"]}, "content":"tere terre"}' \
         https://smart-search.tartunlp.ai/api/vm/generator/process | jq
+
+----------------------------------------------
+
+5 DockerHubis oleva konteineri lisamine KUBERNETESesse
+5.1 Vaikeväärtustega `deployment`-konfiguratsioonifaili loomine
+    $ kubectl create deployment smart-search-api-vm-vmetsjson \
+        --image=tilluteenused/api_vm_vmetsjson:2024.01.22
+
+Keskkonnamuutuja abil saab muuta maksimaalse lubatava päringu suurust.
+Ava konfiguratsioonifail redaktoris
+    $ kubectl edit deployment smart-search-api-vm-vmetsjson
+
+Lisades sinna soovitud keskkonnamuutujate väärtused:
+    env:
+    - name: MAX_CONTENT_LENGTH
+      value: "5000000"
+        
+5.2 Vaikeväärtustega `service`-konfiguratsioonifaili loomine
+    $ kubectl expose deployment smart-search-api-vm-vmetsjson \
+        --type=ClusterIP --port=80 --target-port=7008
+
+5.3 `ingress`-konfiguratsioonifaili täiendamine
+    $ kubectl edit ingress smart-search-api-ingress
+
+Lisa sinna
+    - backend:
+        service:
+        name: smart-search-api-vm-vmetsjson
+        port:
+            number: 80
+    path: /api/vm/generator/?(.*)
+    pathType: Prefix
+
 ----------------------------------------------
 """
 
 import subprocess
 import json
 import argparse
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify, make_response, abort
+from functools import wraps
 
 proc = subprocess.Popen(['./vmetsjson', '--path=.'],  
                             universal_newlines=True, 
@@ -78,7 +111,45 @@ proc = subprocess.Popen(['./vmetsjson', '--path=.'],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.DEVNULL)
 
-app = Flask("vmetsjson")
+app = Flask(__name__)
+
+VERSION = "2024.01.22"
+
+# JSONsisendi max suuruse piiramine {{
+try:
+    MAX_CONTENT_LENGTH=int(os.environ.get('MAX_CONTENT_LENGTH'))
+except:
+    MAX_CONTENT_LENGTH = 5 * 1000000000 # 5 GB 
+
+def limit_content_length(max_length):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            cl = request.content_length
+            if cl is not None and cl > max_length:
+                abort(413)
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
+# }}
+
+@app.errorhandler(413) # Request Entity Too Large: The data value transmitted exceeds the capacity limit.
+def request_entity_too_large(e):
+    return jsonify(error=str(e)), 413
+
+@app.errorhandler(404) # The requested URL was not found on the server.
+def page_not_found(e):
+    return jsonify(error=str(e)), 404
+
+@app.errorhandler(400) # Rotten JSON
+def rotten_json(e):
+    return jsonify(error=str(e)), 400
+
+@app.errorhandler(500) # Internal Error
+def rotten_json(e):
+    return jsonify(error=str(e)), 500
+
+#---------------------------------------------------------------------------
 
 @app.route('/api/vm/generator/version', methods=['GET', 'POST'])
 @app.route('/version', methods=['GET', 'POST'])
@@ -98,9 +169,19 @@ def morf():
     Returns:
         ~flask.Response: Morf sünteesi tulemused
     """
-    proc.stdin.write(f'{json.dumps(request.json)}\n')
-    proc.stdin.flush()
-    return jsonify(json.loads(proc.stdout.readline()))
+    try:
+        request_json = json.loads(request.data)
+    except ValueError as e:
+        abort(400, description=str(e))
+    if ("content" not in request_json) or ("tss" not in request_json):
+        abort(400, description="Required 'content' or 'tss'")
+    try:    
+        proc.stdin.write(f'{json.dumps(request_json)}\n')
+        proc.stdin.flush()
+        response_json = json.loads(proc.stdout.readline())
+    except ValueError as e:
+        abort(500, description=str(e))    
+    return jsonify(response_json), 200
 
 if __name__ == '__main__':
     default_port=7008
