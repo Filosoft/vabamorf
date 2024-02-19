fd085c7f36bedb2b701767f33ac1845d204a5937
Tarmo Vaino <tarmo.vaino@gmail.com>
2024-02-19 20:44:49 +0200

    üht,teist

---
+++ apps/cmdline/vmetyjson/demo-workflow2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo '{"params": {"vmetajson": [ "--guess", "--classic2"]}, "content": "Mees peeti kinni. Telef +372 56 34566"}' \
+| curl --silent --request POST --header "Content-Type: application/json" --data @/dev/stdin https://vabamorf.tartunlp.ai/api/estnltk/tokenizer/process \
+| curl --silent --request POST --header "Content-Type: application/json" --data @/dev/stdin https://vabamorf.tartunlp.ai/api/vm/analyser/process \
+| ../project/unix/vmetyjson \
+| jq
+
+
+
