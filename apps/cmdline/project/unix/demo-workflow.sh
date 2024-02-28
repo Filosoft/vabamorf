e1e7ff210e160da36aea2dbe720b6b6fedf6ed41
Tarmo Vaino <tarmo.vaino@gmail.com>
2024-02-28 15:34:42 +0200

    üht,teist

--- apps/cmdline/project/unix/demo-workflow.sh
+++ apps/cmdline/project/unix/demo-workflow.sh
@@ -17,9 +17,8 @@ docker_local()
     echo '{"params": {"vmetajson": ["--result-string-as-joined", "--result-string-as-discrete", "--stem", "--guess"]}, "content": "Mees peeti kinni. Töx-Mõx, telef +373 45 676 987."}' \
     | curl --silent --request POST --header "Content-Type: application/json" --data @/dev/stdin localhost:6000/api/estnltk/tokenizer/process \
     | curl --silent --request POST --header "Content-Type: application/json" --data @/dev/stdin localhost:7007/api/vm/analyser/process \
-    
-    #| curl --silent --request POST --header "Content-Type: application/json" --data @/dev/stdin localhost:7009/api/vm/disambiguator/process \
-    #| jq 
+    | curl --silent --request POST --header "Content-Type: application/json" --data @/dev/stdin localhost:7009/api/vm/disambiguator/process \
+    | jq 
 
 }
 
